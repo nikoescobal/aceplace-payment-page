bf1197fdd48c638523f1080849705e2e2cb54a5b
Niko Escobal <niko.escobal@gmail.com>
2022-03-09 02:26:16 +0800

    Remove border radius from image, improve styling of all elements

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/card-background.svg":[["card-background.b8d61582.svg","assets/card-background.svg"],"assets/card-background.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
